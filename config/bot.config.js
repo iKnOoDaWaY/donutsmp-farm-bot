dc7aaf17c212c24d37d483f6f97c9a517eb4d8e2
10ftdeep <41876445+iKnOoDaWaY@users.noreply.github.com>
2026-02-02 23:49:14 -0600

    Update server version to 1.21

--- config/bot.config.js
+++ config/bot.config.js
@@ -11,6 +11,6 @@ module.exports = {
     // Change these values to match the server you want the bots to connect to
     host: 'donutsmp.net',
     port: 25565,
-    version: '1.20.4'
+    version: '1.21'
   }
 };
